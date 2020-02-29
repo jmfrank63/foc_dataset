8fa53de6d4cbdc69bca2cfae22b62947d23f65c6
Johannes Maria Frank <jmfrank63@gmail.com>
2020-02-29 06:01:31 -0800

    Added name argument

--- create_dataset.py
+++ create_dataset.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 # Read in a file and place it into a dictionary
 
 from typing import Tuple
+import argparse
 
 FILE = 'TEXTC.txt'
-NAME = "JOHANNESFRANK"
 
 
 def read_to_dict(file_name: str = FILE) -> dict:
@@ -21,26 +22,26 @@ def encode_letter(letter: str) -> int:
     return ord(letter.upper()) - 64
 
 
-def encode_name(name : str) -> int:
+def encode_name(name: str) -> int:
     """ Encode a name to numbers and sum up the numbers """
     return sum([encode_letter(letter) for letter in name])
 
 
-def get_position_and_line_number(encode_number : int) -> Tuple[int, int]:
+def get_position_and_line_number(encode_number: int) -> Tuple[int, int]:
     """ Calculate the line number for the encoded number """
-    line : int = encode_number % 100 + 1
-    pos : int = encode_number // 100
+    line: int = encode_number % 100 + 1
+    pos: int = encode_number // 100
     if pos == 1:
         pos = 4
     elif pos >= 2:
         pos = 9
-    return  pos, line
-    
+    return pos, line
 
-def make_data_set(pos : int, line: int, text_dict : dict, num : int = 15) -> list:
+
+def make_data_set(pos: int, line: int, text_dict: dict, num: int = 15) -> list:
     """ Create a unique data set with four letter words from 
     the given dictionary of words"""
-    word_list : list = []
+    word_list: list = []
     i = pos
     while (True):
         if len(word_list) >= num:
@@ -59,8 +60,12 @@ def make_data_set(pos : int, line: int, text_dict : dict, num : int = 15) -> lis
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', help="Name for your dataset")
+    args = parser.parse_args()
+
     td = read_to_dict()
-    enum = encode_name(NAME)
+    enum = encode_name(args.name)
     pos, line = get_position_and_line_number(enum)
     print(make_data_set(pos, line, td, 15))
 
