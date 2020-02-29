bedf9e062b5c86bc2e9b8ea32a7c5a5519cbad9b
Johannes Maria Frank <jmfrank63@gmail.com>
2020-02-29 06:43:27 -0800

    Added formating for test

--- test_create_dataset.py
+++ test_create_dataset.py
@@ -8,6 +8,7 @@ CONTENT = """TEXT This is the header
 4 This is his(her) text of crappy words
 5 Here are 10000 words"""
 
+
 @pytest.fixture
 def text_dict(tmp_path):
     directory = tmp_path / "sub"
@@ -30,9 +31,11 @@ def test_create_file(tmp_path):
     assert len(text_dict) == len(CONTENT.split('\n'))
     assert text_dict[3] == tuple('This is the third line'.split(' '))
 
+
 def test_encode_letter():
     assert encode_letter('a') == 1
     assert encode_letter('Z') == 26
+
 
 def test_encode_name():
     assert encode_name('A') == 1
@@ -47,10 +50,14 @@ def test_get_position_and_line_number():
     assert get_position_and_line_number(800) == (9, 1)
     assert get_position_and_line_number(999) == (9, 100)
 
-def test_make_data_set(text_dict):
-    assert make_data_set(0, 1, text_dict, 4) == ['this', 'firs', 'line', 'seco']
-    assert make_data_set(4, 1, text_dict, 4) == ['line', 'this','seco', 'thir'] 
-    assert make_data_set(0, 2, text_dict, 4) == ['this','seco', 'line', 'thir']
-    assert make_data_set(9, 2, text_dict, 5) == ['this', 'thir', 'line', 'text', 'crap']
-    assert make_data_set(0, 5, text_dict, 2) == ['here', 'word'] 
 
+def test_make_data_set(text_dict):
+    assert make_data_set(0, 1, text_dict,
+                         4) == ['this', 'firs', 'line', 'seco']
+    assert make_data_set(4, 1, text_dict,
+                         4) == ['line', 'this', 'seco', 'thir']
+    assert make_data_set(0, 2, text_dict,
+                         4) == ['this', 'seco', 'line', 'thir']
+    assert make_data_set(9, 2, text_dict,
+                         5) == ['this', 'thir', 'line', 'text', 'crap']
+    assert make_data_set(0, 5, text_dict, 2) == ['here', 'word']
