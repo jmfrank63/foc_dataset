7c4b08c93648bcd986db1bd8aba4f63aa5e1c988
Johannes Maria Frank <jmfrank63@gmail.com>
2020-02-29 05:20:46 -0800

    First version of data set creation

---
+++ create_dataset.py
@@ -0,0 +1,69 @@
+# Read in a file and place it into a dictionary
+
+from typing import Tuple
+
+FILE = 'TEXTC.txt'
+NAME = "JOHANNESFRANK"
+
+
+def read_to_dict(file_name: str = FILE) -> dict:
+    """ Read the text file into a dictionary"""
+    with open(file_name, 'r') as tfile:
+        text_dict = {
+            num: tuple(line.strip('\n').split(' ')[1:])
+            for num, line in enumerate(tfile)
+        }
+    return text_dict
+
+
+def encode_letter(letter: str) -> int:
+    """ Return the number code for a letter starting with A = 1"""
+    return ord(letter.upper()) - 64
+
+
+def encode_name(name : str) -> int:
+    """ Encode a name to numbers and sum up the numbers """
+    return sum([encode_letter(letter) for letter in name])
+
+
+def get_position_and_line_number(encode_number : int) -> Tuple[int, int]:
+    """ Calculate the line number for the encoded number """
+    line : int = encode_number % 100 + 1
+    pos : int = encode_number // 100
+    if pos == 1:
+        pos = 4
+    elif pos >= 2:
+        pos = 9
+    return  pos, line
+    
+
+def make_data_set(pos : int, line: int, text_dict : dict, num : int = 15) -> list:
+    """ Create a unique data set with four letter words from 
+    the given dictionary of words"""
+    word_list : list = []
+    i = pos
+    while (True):
+        if len(word_list) >= num:
+            break
+        try:
+            word = text_dict[line][i].lower()
+        except IndexError:
+            line += 1
+            i = 0
+            continue
+        word = ''.join(filter(str.isalnum, word))
+        if len(word) >= 4 and word not in word_list:
+            word_list.append(word[:4])
+        i += 1
+    return word_list
+
+
+def main():
+    td = read_to_dict()
+    enum = encode_name(NAME)
+    pos, line = get_position_and_line_number(enum)
+    print(make_data_set(pos, line, td, 15))
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_create_dataset.py
@@ -0,0 +1,52 @@
+import pytest
+from create_dataset import *
+
+CONTENT = """TEXT This is the header
+1 This is the first line
+2 This is the second line
+3 This is the third line"""
+
+@pytest.fixture
+def text_dict(tmp_path):
+    directory = tmp_path / "sub"
+    directory.mkdir()
+    filename = 'text.txt'
+    file_path = directory / filename
+    file_path.write_text(CONTENT)
+    text_dict = read_to_dict(file_path)
+    return text_dict
+
+
+def test_create_file(tmp_path):
+    directory = tmp_path / "sub"
+    directory.mkdir()
+    filename = 'text.txt'
+    file_path = directory / filename
+    file_path.write_text(CONTENT)
+    text_dict = read_to_dict(file_path)
+    assert file_path.read_text() == CONTENT
+    assert len(text_dict) == len(CONTENT.split('\n'))
+    assert text_dict[3] == tuple('This is the third line'.split(' '))
+
+def test_encode_letter():
+    assert encode_letter('a') == 1
+    assert encode_letter('Z') == 26
+
+def test_encode_name():
+    assert encode_name('A') == 1
+    assert encode_name('abcdefgh') == 36
+
+
+def test_get_position_and_line_number():
+    assert get_position_and_line_number(59) == (0, 60)
+    assert get_position_and_line_number(99) == (0, 100)
+    assert get_position_and_line_number(100) == (4, 1)
+    assert get_position_and_line_number(136) == (4, 37)
+    assert get_position_and_line_number(800) == (9, 1)
+    assert get_position_and_line_number(999) == (9, 100)
+
+def test_make_data_set(text_dict):
+    assert make_data_set(0, 1, text_dict, 4) == ['this', 'firs', 'line', 'seco']
+    assert make_data_set(4, 1, text_dict, 4) == ['line', 'this','seco', 'thir'] 
+    assert make_data_set(0, 2, text_dict, 4) == ['this','seco', 'line', 'thir']   
+
